55a72e6af7fac072fcb8190edbb804b7d947286d
Baillie11 <andrewgbaillie@hotmail.com>
2021-12-30 21:53:25 +1100

    Initial Commit
    Able to select file, upload, preview image, change file size and download

---
+++ main.js
@@ -0,0 +1,60 @@
+const canvas = document.getElementById("result");
+const ctx = canvas.getContext("2d");
+
+
+
+function loadImage(event) {
+  const image = document.getElementById("imgDisplayed");
+   image.src = URL.createObjectURL(event.target.files[0]);
+   image.onload = function() {
+       iheight=this.height;
+         iwidth=this.width;
+         console.log(iheight);
+            console.log(iwidth);
+            document.getElementById("imHeight").value=iheight;
+            document.getElementById("imWidth").value=iwidth;
+            
+            
+        };
+}
+
+const preview = document.getElementById("preview");
+preview.addEventListener("click", prev);
+
+function prev() {
+  const image = document.getElementById("imgDisplayed");
+
+  const imWidth = document.getElementById("imWidth").value;
+  const imHeight = document.getElementById("imHeight").value;
+    
+  canvas.width = imWidth;
+  canvas.height = imHeight;
+
+  ctx.drawImage(image, 0, 0, imWidth, imHeight);
+
+//   form = document.querySelector("form");
+//   form.reset();
+}
+
+
+const down = document.querySelector("#down");
+down.addEventListener("click", downloadImage);
+
+function downloadImage(){
+  if(window.navigator.msSaveBlob) {
+    window.navigator.msSaveBlob(canvas.msToBlob(), "resizedImage.png");
+
+  } else {
+
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = canvas.toDataURL();
+
+    a.download = "resizedImage.png";
+    a.click();
+
+    document.body.removeChild(a);
+  }
+
+  window.location.reload();
+}   
